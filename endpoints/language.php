22dc292ef95b49e1d42e4edd1e6d42558bbb5e2e
Rich Fabian <fabian@ix.netcom.com>
2024-11-22 14:12:07 -0500

    fix code spacing

--- endpoints/language.php
+++ endpoints/language.php
@@ -28,8 +28,8 @@ header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-ch
 header("Pragma: no-cache");
 
 if(isset($_GET['debug']) && $_GET['debug']) {
-	print_r(__vbox_language::$langdata);
-	return;
+    print_r(__vbox_language::$langdata);
+    return;
 }
 
 /*
@@ -50,72 +50,71 @@ function trans(s,c,n,h) {
         h = c.shift();
         c = o;
     }
-	if(!c) c = 'VBoxGlobal';
 
-	var r = transreal(s,c,n,h);
+    if(!c) c = 'VBoxGlobal';
+    var r = transreal(s,c,n,h);
+    if(typeof r != 'string') {
+       // console.log('Could not translate ' + s + ' with ' + c);
+       return s;
+    }
 
-	if(typeof r != 'string') {
-	   // console.log('Could not translate ' + s + ' with ' + c);
-	   return s;
-	}
-
-	return r;
+    return r;
 }
 
+
 function transreal(w,context,number,comment) {
+    try {
 
-	try {
+        if(__vboxLangData['contexts'][context]['messages'][w]['translation']) {
 
-		if(__vboxLangData['contexts'][context]['messages'][w]['translation']) {
+            if(__vboxLangData['contexts'][context]['messages'][w]['translation']['numerusform']) {
 
-			if(__vboxLangData['contexts'][context]['messages'][w]['translation']['numerusform']) {
+                var t = __vboxLangData['contexts'][context]['messages'][w]['translation']['numerusform'];
 
-				var t = __vboxLangData['contexts'][context]['messages'][w]['translation']['numerusform'];
+                if(!number) number = 1;
 
-				if(!number) number = 1;
+                if(number <= 1 && t[0]) return t[0];
+                if(number > 1 && t[1]) return t[1];
+                if(t[0]) return t[0];
+                return t[1];
+            }
+            /*
+            if (__vboxLangData['contexts'][context]['messages'][w] && __vboxLangData['contexts'][context]['messages'][w]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w]['translation_attr']['type'] == 'obsolete') {
+               console.log(w + ' in ' + context + ' is obsolete');
+            }
+            */
+            return __vboxLangData['contexts'][context]['messages'][w]['translation'];
 
-				if(number <= 1 && t[0]) return t[0];
-				if(number > 1 && t[1]) return t[1];
-				if(t[0]) return t[0];
-				return t[1];
-			}
-			/*
-			if (__vboxLangData['contexts'][context]['messages'][w] && __vboxLangData['contexts'][context]['messages'][w]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w]['translation_attr']['type'] == 'obsolete') {
-			   console.log(w + ' in ' + context + ' is obsolete');
-			}
-			*/
-			return __vboxLangData['contexts'][context]['messages'][w]['translation'];
+        } else if(__vboxLangData['contexts'][context]['messages'][w][0]) {
 
-		} else if(__vboxLangData['contexts'][context]['messages'][w][0]) {
+            if(comment) {
+                for(var i in __vboxLangData['contexts'][context]['messages'][w]) {
+                    if(__vboxLangData['contexts'][context]['messages'][w][i]['comment'] == comment) {
+                        /*
+                        if (__vboxLangData['contexts'][context]['messages'][w][i]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w][i]['translation_attr']['type'] == 'obsolete') {
+                            console.log(w + ' ' + ' and ' + comment + ' is obsolete');
+                         }
+                         */
 
-			if(comment) {
-				for(var i in __vboxLangData['contexts'][context]['messages'][w]) {
-					if(__vboxLangData['contexts'][context]['messages'][w][i]['comment'] == comment) {
-					    /*
-						if (__vboxLangData['contexts'][context]['messages'][w][i]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w][i]['translation_attr']['type'] == 'obsolete') {
-						    console.log(w + ' ' + ' and ' + comment + ' is obsolete');
-			             }
-			             */
+                        return __vboxLangData['contexts'][context]['messages'][w][i]['translation'];
+                    }
+                }
+            }
+            /*
+            if (__vboxLangData['contexts'][context]['messages'][w][0] && __vboxLangData['contexts'][context]['messages'][w][0]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w][0]['translation_attr']['type'] == 'obsolete') {
+               console.log(w + ' in ' + context + ' is obsolete');
+            }
+            */
 
-					    return __vboxLangData['contexts'][context]['messages'][w][i]['translation'];
-					}
-				}
-			}
-			/*
-			if (__vboxLangData['contexts'][context]['messages'][w][0] && __vboxLangData['contexts'][context]['messages'][w][0]['translation_attr'] && __vboxLangData['contexts'][context]['messages'][w][0]['translation_attr']['type'] == 'obsolete') {
-			   console.log(w + ' in ' + context + ' is obsolete');
-			}
-			*/
+            return __vboxLangData['contexts'][context]['messages'][w][0]['translation'];
 
-			return __vboxLangData['contexts'][context]['messages'][w][0]['translation'];
+        } else {
+            return w;
+        }
 
-		} else {
-			return w;
-		}
-
-	} catch(err) {
-		// console.log(w + ' - ' + context + ': ' + err);
-		return w;
-	}
+    } catch(err) {
+        // console.log(w + ' - ' + context + ': ' + err);
+        return w;
+    }
 }
 
