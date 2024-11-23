bcc8fc1b44d93db109f113bb3ef714bda88a46a9
Rich Fabian <fabian@ix.netcom.com>
2024-11-22 19:01:16 -0500

    fix code spacing

--- endpoints/api.php
+++ endpoints/api.php
@@ -53,280 +53,277 @@ $vbox = null; // May be set during request handling
  */
 try {
 
-	/* Check for password recovery file */
-	if(file_exists(dirname(dirname(__FILE__)).'/recovery.php')) {
-		throw new Exception('recovery.php exists in VirtualBox Web Manager\'s folder. This is a security hazard. VirtualBox Web Manager will not run until recovery.php has been renamed to a file name that does not end in .php such as <b>recovery.php-disabled</b>.',vboxconnector::VBWEBMGR_ERRNO_FATAL);
-	}
-
-	/* Check for PHP version */
-	if (!version_compare(PHP_VERSION, '5.2.0', '>=')) {
-		throw new Exception('VirtualBox Web Manager requires PHP >= 5.2.0, but this server is running version '. PHP_VERSION .'. Please upgrade PHP.');
-	}
-
-	# Only valid function chars
-	$request['fn'] = preg_replace('[^a-zA-Z0-9_-]', '', $request['fn']);
+    /* Check for password recovery file */
+    if(file_exists(dirname(dirname(__FILE__)).'/recovery.php')) {
+        throw new Exception('recovery.php exists in VirtualBox Web Manager\'s folder. This is a security hazard. VirtualBox Web Manager will not run until recovery.php has been renamed to a file name that does not end in .php such as <b>recovery.php-disabled</b>.',vboxconnector::VBWEBMGR_ERRNO_FATAL);
+    }
+
+    /* Check for PHP version */
+    if (!version_compare(PHP_VERSION, '5.2.0', '>=')) {
+        throw new Exception('VirtualBox Web Manager requires PHP >= 5.2.0, but this server is running version '. PHP_VERSION .'. Please upgrade PHP.');
+    }
+
+    # Only valid function chars
+    $request['fn'] = preg_replace('[^a-zA-Z0-9_-]', '', $request['fn']);
+
+    /* Check for function called */
+    switch($request['fn']) {
+
+        /*
+         * No method called
+         */
+        case '':
+            throw new Exception('No method called.');
+            break;
+
+        /*
+         * Return vboxwebmgr's configuration data
+         */
+        case 'getConfig':
+
+            $settings = new vboxwebmgrConfigClass();
+            $response['data']['responseData'] = get_object_vars($settings);
+            $response['data']['responseData']['host'] = parse_url($response['data']['responseData']['location']);
+            $response['data']['responseData']['host'] = $response['data']['responseData']['host']['host'];
+            $response['data']['responseData']['vboxwebmgr'] = @constant('VBOXWEBMGR_VER');
+
+            // Session
+            session_init();
+
+            // Hide credentials
+            unset($response['data']['responseData']['username']);
+            unset($response['data']['responseData']['password']);
+            foreach($response['data']['responseData']['servers'] as $k => $v)
+                $response['data']['responseData']['servers'][$k] = array('name'=>$v['name']);
+
+            // Vbox version
+            $vbox = new vboxconnector();
+            $response['data']['responseData']['version'] = $vbox->getVersion();
+            $response['data']['responseData']['hostOS'] = $vbox->vbox->host->operatingSystem;
+            $response['data']['responseData']['DSEP'] = $vbox->getDsep();
+            $response['data']['responseData']['groupDefinitionKey'] = ($settings->vboxwebmgrGroups ? vboxconnector::vboxwebmgrGroupKey : 'GUI/GroupDefinitions');
+
+            $response['data']['success'] = true;
 
-	/* Check for function called */
-	switch($request['fn']) {
+            break;
+
+        /*
+         *
+         * USER FUNCTIONS FOLLOW
+         *
+         */
+
+        /*
+         * Pass login to authentication module.
+         */
+        case 'login':
 
-		/*
-		 * No method called
-		 */
-	    case '':
-	    	throw new Exception('No method called.');
-	    	break;
+            // NOTE: Do not break. Fall through to 'getSession
+            if(!$request['params']['u'] || !$request['params']['p']) {
+                break;
+            }
 
-		/*
-		 * Return vboxwebmgr's configuration data
-		 */
-		case 'getConfig':
+            // Session
+            session_init(true);
 
-			$settings = new vboxwebmgrConfigClass();
-			$response['data']['responseData'] = get_object_vars($settings);
-			$response['data']['responseData']['host'] = parse_url($response['data']['responseData']['location']);
-			$response['data']['responseData']['host'] = $response['data']['responseData']['host']['host'];
-			$response['data']['responseData']['vboxwebmgr'] = @constant('VBOXWEBMGR_VER');
+            $settings = new vboxwebmgrConfigClass();
+
+            // Try / catch here to hide login credentials
+            try {
+                $settings->auth->login($request['params']['u'], $request['params']['p']);
+            } catch(Exception $e) {
+                throw new Exception($e->getMessage(), $e->getCode());
+            }
+
+            // We're done writing to session
+            if(function_exists('session_write_close'))
+                @session_write_close();
+
+        /*
+         * Return $_SESSION data
+         */
+        case 'getSession':
 
-			// Session
-			session_init();
+            $settings = new vboxwebmgrConfigClass();
+            if(method_exists($settings->auth,'autoLoginHook'))
+            {
+                // Session
+                session_init(true);
 
-			// Hide credentials
-			unset($response['data']['responseData']['username']);
-			unset($response['data']['responseData']['password']);
-			foreach($response['data']['responseData']['servers'] as $k => $v)
-				$response['data']['responseData']['servers'][$k] = array('name'=>$v['name']);
+                $settings->auth->autoLoginHook();
 
-			// Vbox version
-			$vbox = new vboxconnector();
-			$response['data']['responseData']['version'] = $vbox->getVersion();
-			$response['data']['responseData']['hostOS'] = $vbox->vbox->host->operatingSystem;
-			$response['data']['responseData']['DSEP'] = $vbox->getDsep();
-			$response['data']['responseData']['groupDefinitionKey'] = ($settings->vboxwebmgrGroups ? vboxconnector::vboxwebmgrGroupKey : 'GUI/GroupDefinitions');
+                // We're done writing to session
+                if(function_exists('session_write_close'))
+                    @session_write_close();
 
-			$response['data']['success'] = true;
+            } else {
 
-			break;
+                session_init();
 
-		/*
-		 *
-		 * USER FUNCTIONS FOLLOW
-		 *
-		 */
+            }
 
-		/*
-		 * Pass login to authentication module.
-		 */
-		case 'login':
 
+            $response['data']['responseData'] = $_SESSION;
+            $response['data']['success'] = true;
+            break;
 
-			// NOTE: Do not break. Fall through to 'getSession
-			if(!$request['params']['u'] || !$request['params']['p']) {
-				break;
-			}
+        /*
+         * Change vboxwebmgr password. Passed to auth module's
+         * changePassword method.
+         */
+        case 'changePassword':
 
-			// Session
-			session_init(true);
+            // Session
+            session_init(true);
 
-			$settings = new vboxwebmgrConfigClass();
+            $settings = new vboxwebmgrConfigClass();
+            $response['data']['success'] = $settings->auth->changePassword($request['params']['old'],
+                $request['params']['new']);
 
-			// Try / catch here to hide login credentials
-			try {
-				$settings->auth->login($request['params']['u'], $request['params']['p']);
-			} catch(Exception $e) {
-				throw new Exception($e->getMessage(), $e->getCode());
-			}
+            // We're done writing to session
+            if(function_exists('session_write_close'))
+                @session_write_close();
 
-			// We're done writing to session
-			if(function_exists('session_write_close'))
-				@session_write_close();
+            break;
 
+        /*
+         * Get a list of vboxwebmgr users. Passed to auth module's
+         * getUsers method.
+         */
+        case 'getUsers':
 
+            // Session
+            session_init();
 
-		/*
-		 * Return $_SESSION data
-		 */
-		case 'getSession':
+            // Must be an admin
+            if(!$_SESSION['admin']) break;
 
-			$settings = new vboxwebmgrConfigClass();
-			if(method_exists($settings->auth,'autoLoginHook'))
-			{
-				// Session
-				session_init(true);
+            $settings = new vboxwebmgrConfigClass();
+            $response['data']['responseData'] = $settings->auth->listUsers();
+            $response['date']['success'] = true;
 
-				$settings->auth->autoLoginHook();
+            break;
 
-				// We're done writing to session
-				if(function_exists('session_write_close'))
-					@session_write_close();
+        /*
+         * Remove a vboxwebmgr user. Passed to auth module's
+         * deleteUser method.
+         */
+        case 'delUser':
 
-			} else {
+            // Session
+            session_init();
 
-				session_init();
+            // Must be an admin
+            if(!$_SESSION['admin']) break;
 
-			}
+            $settings = new vboxwebmgrConfigClass();
+            $settings->auth->deleteUser($request['params']['u']);
 
+            $response['data']['success'] = true;
+            break;
 
-			$response['data']['responseData'] = $_SESSION;
-			$response['data']['success'] = true;
-			break;
+        /*
+         * Edit a vboxwebmgr user. Passed to auth module's
+         * updateUser method.
+         */
+        case 'editUser':
 
-		/*
-		 * Change vboxwebmgr password. Passed to auth module's
-		 * changePassword method.
-		 */
-		case 'changePassword':
+            $skipExistCheck = true;
+            // Fall to addUser
 
-			// Session
-			session_init(true);
+        /*
+         * Add a user to vboxwebmgr. Passed to auth module's
+         * updateUser method.
+         */
+        case 'addUser':
 
-			$settings = new vboxwebmgrConfigClass();
-			$response['data']['success'] = $settings->auth->changePassword($request['params']['old'],
-					                                                       $request['params']['new']);
+            // Session
+            session_init();
 
-			// We're done writing to session
-			if(function_exists('session_write_close'))
-				@session_write_close();
+            // Must be an admin
+            if(!$_SESSION['admin']) break;
 
-			break;
+            $settings = new vboxwebmgrConfigClass();
+            $settings->auth->updateUser($request['params'], @$skipExistCheck);
 
-		/*
-		 * Get a list of vboxwebmgr users. Passed to auth module's
-		 * getUsers method.
-		 */
-		case 'getUsers':
+            $response['data']['success'] = true;
+            break;
 
-			// Session
-			session_init();
+        /*
+         * Log out of vboxwebmgr. Passed to auth module's
+         * logout method.
+         */
+        case 'logout':
 
-			// Must be an admin
-			if(!$_SESSION['admin']) break;
+            // Session
+            session_init(true);
 
-			$settings = new vboxwebmgrConfigClass();
-			$response['data']['responseData'] = $settings->auth->listUsers();
-			$response['date']['success'] = true;
+            $vbox = new vboxconnector();
+            $vbox->skipSessionCheck = true;
 
-			break;
+            $settings = new vboxwebmgrConfigClass();
+            $settings->auth->logout($response);
 
-		/*
-		 * Remove a vboxwebmgr user. Passed to auth module's
-		 * deleteUser method.
-		 */
-		case 'delUser':
+            session_destroy();
 
-			// Session
-			session_init();
+            $response['data']['success'] = true;
 
-			// Must be an admin
-			if(!$_SESSION['admin']) break;
+            break;
 
-			$settings = new vboxwebmgrConfigClass();
-			$settings->auth->deleteUser($request['params']['u']);
 
-			$response['data']['success'] = true;
-			break;
+        /*
+         * If the above cases did not match, assume it is a request
+         * that should be passed to vboxconnector.
+         */
+        default:
 
-		/*
-		 * Edit a vboxwebmgr user. Passed to auth module's
-		 * updateUser method.
-		 */
-		case 'editUser':
+            $vbox = new vboxconnector();
 
-			$skipExistCheck = true;
-			// Fall to addUser
 
-		/*
-		 * Add a user to vboxwebmgr. Passed to auth module's
-		 * updateUser method.
-		 */
-		case 'addUser':
+            /*
+             * Every 1 minute we'll check that the account has not
+             * been deleted since login, and update admin credentials.
+             */
+            if($_SESSION['user'] && ((intval($_SESSION['authCheckHeartbeat'])+60) < time())) {
 
-			// Session
-			session_init();
+                // init session and keep it open
+                session_init(true);
+                $vbox->settings->auth->heartbeat($vbox);
 
-			// Must be an admin
-			if(!$_SESSION['admin']) break;
+                // We're done writing to session
+                if(function_exists('session_write_close'))
+                    @session_write_close();
 
-			$settings = new vboxwebmgrConfigClass();
-			$settings->auth->updateUser($request['params'], @$skipExistCheck);
+            } else {
 
-			$response['data']['success'] = true;
-			break;
+                // init session but close it
+                session_init();
 
-		/*
-		 * Log out of vboxwebmgr. Passed to auth module's
-		 * logout method.
-		 */
-		case 'logout':
+            }
 
-			// Session
-			session_init(true);
+            /*
+             *  Persistent request data
+             */
+            if(is_array($request['persist'])) {
+                $vbox->persistentRequest = $request['persist'];
+            }
 
-			$vbox = new vboxconnector();
-			$vbox->skipSessionCheck = true;
 
-			$settings = new vboxwebmgrConfigClass();
-			$settings->auth->logout($response);
+            /*
+             * Call to vboxconnector
+             */
+            $vbox->{$request['fn']}($request['params'],array(&$response));
 
-			session_destroy();
 
-			$response['data']['success'] = true;
+            /*
+             * Send back persistent request in response
+            */
+            if(is_array($vbox->persistentRequest) && count($vbox->persistentRequest)) {
+                $response['data']['persist'] = $vbox->persistentRequest;
+            }
+            break;
 
-			break;
-
-
-		/*
-		 * If the above cases did not match, assume it is a request
-		 * that should be passed to vboxconnector.
-		 */
-		default:
-
-			$vbox = new vboxconnector();
-
-
-			/*
-			 * Every 1 minute we'll check that the account has not
-			 * been deleted since login, and update admin credentials.
-			 */
-			if($_SESSION['user'] && ((intval($_SESSION['authCheckHeartbeat'])+60) < time())) {
-
-				// init session and keep it open
-				session_init(true);
-				$vbox->settings->auth->heartbeat($vbox);
-
-				// We're done writing to session
-				if(function_exists('session_write_close'))
-					@session_write_close();
-
-			} else {
-
-				// init session but close it
-				session_init();
-
-			}
-
-			/*
-			 *  Persistent request data
-			 */
-			if(is_array($request['persist'])) {
-				$vbox->persistentRequest = $request['persist'];
-			}
-
-
-			/*
-			 * Call to vboxconnector
-			 */
-			$vbox->{$request['fn']}($request['params'],array(&$response));
-
-
-			/*
-			 * Send back persistent request in response
-			*/
-			if(is_array($vbox->persistentRequest) && count($vbox->persistentRequest)) {
-				$response['data']['persist'] = $vbox->persistentRequest;
-			}
-			break;
-
-	} // </switch()>
+    } // </switch()>
 
 /*
  * Catch all exceptions and populate errors in the
@@ -334,46 +331,47 @@ try {
  */
 } catch (Exception $e) {
 
-	// Just append to $vbox->errors and let it get
-	// taken care of below
-	if(!$vbox || !$vbox->errors) {
-		$vbox->errors = array();
-	}
-	$vbox->errors[] = $e;
+    // Just append to $vbox->errors and let it get
+    // taken care of below
+    if(!$vbox || !$vbox->errors) {
+        $vbox->errors = array();
+    }
+    $vbox->errors[] = $e;
 }
 
 
 // Add any messages
 if($vbox && count($vbox->messages)) {
-	foreach($vbox->messages as $m)
-		$response['messages'][] = 'vboxconnector('.$request['fn'] .'): ' . $m;
+    foreach($vbox->messages as $m)
+        $response['messages'][] = 'vboxconnector('.$request['fn'] .'): ' . $m;
 }
+
 // Add other error info
 if($vbox && $vbox->errors) {
 
-	foreach($vbox->errors as $e) { /* @var $e Exception */
+    foreach($vbox->errors as $e) { /* @var $e Exception */
 
-		ob_start();
-		print_r($e);
-		$d = ob_get_contents();
-		ob_end_clean();
+        ob_start();
+        print_r($e);
+        $d = ob_get_contents();
+        ob_end_clean();
 
-		# Add connection details to connection errors
-		if($e->getCode() == vboxconnector::VBWEBMGR_ERRNO_CONNECT && isset($vbox->settings))
-			$d .= "\n\nLocation:" . $vbox->settings->location;
+        # Add connection details to connection errors
+        if($e->getCode() == vboxconnector::VBWEBMGR_ERRNO_CONNECT && isset($vbox->settings))
+            $d .= "\n\nLocation:" . $vbox->settings->location;
 
-		$response['messages'][] = htmlentities($e->getMessage()).' ' . htmlentities($details);
+        $response['messages'][] = htmlentities($e->getMessage()).' ' . htmlentities($details);
 
-		$response['errors'][] = array(
-			'error'=> ($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_HTML ? $e->getMessage() : htmlentities($e->getMessage())),
-			'details'=>htmlentities($d),
-			'errno'=>$e->getCode(),
-			// Fatal errors halt all processing
-			'fatal'=>($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_FATAL),
-			// Connection errors display alternate servers options
-			'connection'=>($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_CONNECT)
-		);
-	}
+        $response['errors'][] = array(
+            'error'=> ($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_HTML ? $e->getMessage() : htmlentities($e->getMessage())),
+            'details'=>htmlentities($d),
+            'errno'=>$e->getCode(),
+            // Fatal errors halt all processing
+            'fatal'=>($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_FATAL),
+            // Connection errors display alternate servers options
+            'connection'=>($e->getCode() & vboxconnector::VBWEBMGR_ERRNO_CONNECT)
+        );
+    }
 }
 
 /*
@@ -381,9 +379,9 @@ if($vbox && $vbox->errors) {
  * print_r to dump data to browser.
  */
 if(isset($request['printr'])) {
-	print_r($response);
+    print_r($response);
 } else {
     header('Content-type: application/json');
-	echo(json_encode($response));
+    echo(json_encode($response));
 }
 
