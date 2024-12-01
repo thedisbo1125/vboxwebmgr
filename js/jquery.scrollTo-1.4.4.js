ba411b6554068e2a3a4393e51d9e9b1ac8e8d0f4
Rich Fabian <fabian@ix.netcom.com>
2024-12-01 10:03:31 -0500

    fix code spacing

--- js/jquery.scrollTo-1.4.4.js
+++ js/jquery.scrollTo-1.4.4.js
@@ -53,7 +53,7 @@
  */
 
 ;(function( $ ) {
-	
+
 	var $scrollTo = $.scrollTo = function( target, duration, settings ) {
 		$(window).scrollTo( target, duration, settings );
 	};
@@ -81,9 +81,9 @@
 					return elem;
 
 			var doc = (elem.contentWindow || elem).document || elem.ownerDocument || elem;
-			
+
 			return /webkit/i.test(navigator.userAgent) || doc.compatMode == 'BackCompat' ?
-				doc.body : 
+				doc.body :
 				doc.documentElement;
 		});
 	};
@@ -95,16 +95,16 @@
 		}
 		if (typeof settings == 'function')
 			settings = { onAfter:settings };
-			
+
 		if (target == 'max')
 			target = 9e9;
-			
+
 		settings = $.extend( {}, $scrollTo.defaults, settings );
 		// Speed is still recognized for backwards compatibility
 		duration = duration || settings.duration;
 		// Make sure the settings are given right
 		settings.queue = settings.queue && settings.axis.length > 1;
-		
+
 		if (settings.queue)
 			// Let's keep the overall duration
 			duration /= 2;
@@ -135,7 +135,7 @@
 				case 'object':
 					// DOMElement / jQuery
 					if (targ.is || targ.style)
-						// Get the real position of the target 
+						// Get the real position of the target
 						toff = (targ = $(targ)).offset();
 			}
 			$.each( settings.axis.split(''), function( i, axis ) {
@@ -153,16 +153,16 @@
 						attr[key] -= parseInt(targ.css('margin'+Pos)) || 0;
 						attr[key] -= parseInt(targ.css('border'+Pos+'Width')) || 0;
 					}
-					
+
 					attr[key] += settings.offset[pos] || 0;
-					
+
 					if(settings.over[pos])
 						// Scroll to a fraction of its width/height
 						attr[key] += targ[axis=='x'?'width':'height']() * settings.over[pos];
-				} else { 
+				} else {
 					var val = targ[pos];
 					// Handle percentage values
-					attr[key] = val.slice && val.slice(-1) == '%' ? 
+					attr[key] = val.slice && val.slice(-1) == '%' ?
 						parseFloat(val) / 100 * max
 						: val;
 				}
@@ -183,7 +183,7 @@
 				}
 			});
 
-			animate( settings.onAfter );			
+			animate( settings.onAfter );
 
 			function animate( callback ) {
 				$elem.animate( attr, duration, settings.easing, callback && function() {
@@ -193,21 +193,21 @@
 
 		}).end();
 	};
-	
+
 	// Max scrolling position, works on quirks mode
 	// It only fails (not too badly) on IE, quirks mode.
 	$scrollTo.max = function( elem, axis ) {
 		var Dim = axis == 'x' ? 'Width' : 'Height',
 			scroll = 'scroll'+Dim;
-		
+
 		if (!$(elem).is('html,body'))
 			return elem[scroll] - $(elem)[Dim.toLowerCase()]();
-		
+
 		var size = 'client' + Dim,
 			html = elem.ownerDocument.documentElement,
 			body = elem.ownerDocument.body;
 
-		return Math.max( html[scroll], body[scroll] ) 
+		return Math.max( html[scroll], body[scroll] )
 			 - Math.min( html[size]  , body[size]   );
 	};
 
