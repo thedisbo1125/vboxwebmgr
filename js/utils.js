10c7e4b49d53728128ce8a8c5b0264ba023be95f
Rich Fabian <fabian@ix.netcom.com>
2024-12-10 10:20:29 -0500

    add ability to change PKI certificate used for RDP on VMs

--- js/utils.js
+++ js/utils.js
@@ -770,6 +770,7 @@ function vboxInitDisplay(root,context) {
 
 			$(roottbl).find('input:not(.vboxEnablerCheckbox,.vboxEnablerIgnore)').prop('disabled',!this.checked);
 			$(roottbl).find('select:not(.vboxEnablerIgnore)').prop('disabled',!this.checked);
+			(this.checked ? $(roottbl).find('label.certfilename').removeClass('vboxcertdisabled') : $(roottbl).find('label.certfilename:not(.vboxEnablerIgnore)').addClass('vboxcertdisabled'));
 			(this.checked ? $(roottbl).find('th').removeClass('vboxDisabled') : $(roottbl).find('th:not(.vboxEnablerIgnore)').addClass('vboxDisabled'));
 			(this.checked ? $(roottbl).find('.vboxEnablerListen').removeClass('vboxDisabled') : $(roottbl).find('.vboxEnablerListen').addClass('vboxDisabled'));
 
@@ -780,6 +781,7 @@ function vboxInitDisplay(root,context) {
 			if (vmrunning == true) {
 				$(roottbl).find('tr:not(.vboxRunningEnabled)').find('span').addClass('disabled');
 				$(roottbl).find('tr:not(.vboxRunningEnabled)').find('input,select,textarea').addClass('disabled').prop('disabled',true);
+                $(roottbl).find('tr:not(.vboxRunningEnabled)').find('label.certfilename').addClass('vboxcertdisabled');
 			}
 
 
