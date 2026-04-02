4fa5442f942f7d326a92355d947568c6d4c16972
Rich Fabian <fabian@ix.netcom.com>
2026-04-02 12:27:31 -0400

    remove autorefresh of drives after taking a snapshot for further testing, produces inconsistent behavior

--- js/vboxwebmgr.js
+++ js/vboxwebmgr.js
@@ -1005,8 +1005,6 @@ var vboxVMDetailsSections = {
 		title: 'Storage',
 		icon: 'hd_16px.png',
 		settingsLink: 'Storage',
-		timeoutisset: false,
-		reloadmedium: false,
 		redrawMachineEvents: ['OnMediumChanged', 'OnMachineStateChanged'],
 		_refreshVMMedia: function(vmid, mid) {
 
@@ -1023,29 +1021,8 @@ var vboxVMDetailsSections = {
 				l.removeLoading();
 			});
 		},
-		_refreshVMMediaNoLoader: function(vmid, mid) {
-
-			if(vmid !== undefined) {
-
-				// See if medium is there
-				var mRefresh = true;
-				var vmidloc = structuredClone(vmid);
-				if(!vboxMedia.getMediumById(mid)) {
-					mRefresh = vboxAjaxRequest('vboxGetMedia');
-				}
-				$.when(mRefresh, vboxVMDataMediator.refreshVMData(vmid)).done(function(d){
-					if(d && d.responseData) {
-						$('#vboxPane').data('vboxMedia',d.responseData);
-					}
-					vboxVMDetailsSections.storage.timeoutisset = false;
-				}).always(function() {
-					$('#vboxPane').trigger('vboxEvents', [[{eventType:'OnMachineDataChanged',machineId:vmidloc}]]);
-				});
-			}
-		},
 		rows: function(d) {
 
-			vboxVMDetailsSections.storage.reloadmedium = false;
 			var rows = new Array();
 
 			for(var a = 0; a < d['storageControllers'].length; a++) {
@@ -1075,9 +1052,6 @@ var vboxVMDetailsSections = {
 							d.id+"','"+d['storageControllers'][a]['mediumAttachments'][b].medium.id+"');\">" +
 								trans('Refresh','UIActionPool')+"</a>";
 
-							vboxVMDetailsSections.storage.reloadmedium = true;
-							var d1 = d.id;
-							var d2 = d['storageControllers'][a]['mediumAttachments'][b].medium.id;
 						} else {
 							portDesc = trans('Refresh','UIActionPool');
 						}
@@ -1101,18 +1075,6 @@ var vboxVMDetailsSections = {
 						data: (d['storageControllers'][a]['mediumAttachments'][b].type == 'DVD' ? trans('[Optical Drive]','UIDetails') + ' ': '') + portDesc,
 						html: true
 					};
-
-				}
-
-				var refreshmedium = function(vmid, mid){
-					vboxVMDetailsSections.storage._refreshVMMediaNoLoader(vmid, mid);
-				}
-
-				if(vboxVMDetailsSections.storage.reloadmedium && vboxVMDetailsSections.storage.timeoutisset == false) {
-					vboxVMDetailsSections.storage.timeoutisset = true;
-
-					vboxVMDetailsSections.storage.reloadmedium = false;
-					setTimeout(refreshmedium(d1,d2), 50);
 
 				}
 
