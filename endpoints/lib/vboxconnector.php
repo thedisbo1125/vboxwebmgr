58d6e7da30825116b86fccaa25e842f1948337ea
Rich Fabian <fabian@ix.netcom.com>
2026-07-06 20:05:15 -0400

    add subtype to return of vboxGetGuestOSTypes function

--- endpoints/lib/vboxconnector.php
+++ endpoints/lib/vboxconnector.php
@@ -3493,6 +3493,7 @@ class vboxconnector {
 				'familyId' => $g->familyId,
 				'familyDescription' => $g->familyDescription,
 				'id' => $g->id,
+				'subtype' => $g->subtype,
 				'description' => $g->description,
 				'is64Bit' => $bit64,
 				'recommendedRAM' => $g->recommendedRAM,
